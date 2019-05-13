9a4f18828a57dfb45820b499bf3e73a4342dab05
allerria <wtf496@gmail.com>
2019-05-13 22:18:22 +0900

    kekas and fbeta loss

--- imet/losses.py
+++ imet/losses.py
@@ -1,4 +1,5 @@
 from torch import nn
+import torch
 
 
 class FocalLoss(nn.Module):
@@ -21,9 +22,41 @@ class FocalLoss(nn.Module):
         return loss.sum(dim=1)
 
 
+
+class FBeta(nn.Module):
+    def __init__(self, threshold=0.1, beta=2, reduction='mean'):
+        super().__init__()
+        self.threshold = threshold
+        self.beta = beta
+        self.reduction = reduction
+
+    def forward(self, input, target):
+        if not (target.size() == input.size()):
+            raise ValueError("Target size ({}) must be the same as input size ({})"
+                             .format(target.size(), input.size()))
+       
+        loss = 0
+        lack_cls = target.sum(dim=0) == 0
+        if lack_cls.any():
+            loss += nn.functional.binary_cross_entropy_with_logits(
+                input, target, reduction=self.reduction)
+        predict = torch.sigmoid(input)
+        predict = torch.clamp(predict * (1-target), min=0.01) + predict * target
+        tp = predict * target
+        tp = tp.sum(dim=0)
+        precision = tp / (predict.sum(dim=0) + 1e-8)
+        recall = tp / (target.sum(dim=0) + 1e-8)
+        f1 = (1 + self.beta ** 2) * (precision * recall / (self.beta ** 2 * precision + recall + 1e-8))
+        if self.reduction == 'mean':
+            f1 = f1.mean()
+        return 1 - f1 + loss
+
+
+
 losses_dict = {
     'bce' :  nn.BCEWithLogitsLoss(reduction='none'),
-    'focal': FocalLoss()
+    'focal': FocalLoss(),
+    'fbeta': FBeta(reduction='none')
 }
 
 
